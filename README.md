a7592999448cf951514533ee30fcd168c95730db
Jason Mulligan <jason.mulligan@avoidwork.com>
2014-07-19 11:23:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# status-board
+
+***status-board*** is a node.js application designed to provide real time server stats to Panic's Status Board dashboard.
+
---
+++ app.js
@@ -0,0 +1,49 @@
+var TurtleIO = require( "turtle.io" ),
+    keigai   = require( "keigai" ),
+    filesize  = require("filesize" ),
+    os       = require( "os" ),
+    config   = require( "./config.json" ),
+    app      = new TurtleIO(),
+    array    = keigai.util.array,
+    csv      = keigai.util.json.csv
+    stats    = [];
+
+function chart () {
+	return stats.reverse();
+}
+
+function table () {
+	return array.last( stats, 10 ).reverse();
+}
+
+function stat () {
+	stats.push( {
+		uptime : os.uptime(),
+		cpu    : os.loadavg()[0].toFixed( 2 ),
+		memory : filesize( os.totalmem() - os.freemem() )
+	} );
+
+	if ( stats.length > 100 ) {
+		stats = array.limit( stats, 0, 100 );
+	}
+}
+
+app.get( "/chart.csv", function ( req, res ) {
+	app.respond( req, res, csv( chart() ), 200, {"content-type": "text/csv"} );
+} );
+
+app.get( "/chart.json", function ( req, res ) {
+	app.respond( req, res, chart() );
+} );
+
+app.get( "/table.csv", function ( req, res ) {
+	app.respond( req, res, csv( table() ), 200, {"content-type": "text/csv"} );
+} );
+
+app.get( "/table.json", function ( req, res ) {
+	app.respond( req, res, table() );
+} );
+
+app.start( config );
+
+setInterval( stat, 1000 );
